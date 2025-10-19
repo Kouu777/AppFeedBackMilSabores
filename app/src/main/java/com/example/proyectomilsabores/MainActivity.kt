08e64b14e5d19bab8fb832d063393aed35b674ca
Kouu <mart.vidalsarav06@gmail.com>
2025-10-19 18:48:46 -0300

    1st commit

---
+++ app/src/main/java/com/example/proyectomilsabores/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.proyectomilsabores
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val btnLogin: Button = findViewById(R.id.btn_login)
+        val etx_paswd: EditText = findViewById(R.id.etx_pass)
+        val etx_user: EditText = findViewById(R.id.etx_user)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
